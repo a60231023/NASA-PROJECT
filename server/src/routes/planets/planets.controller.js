8a5916c161779705d3b1b46380b7d43ecefc8bf5
a60231023 <ayushtiwari6061@gmail.com>
2023-03-04 12:29:24 +0530

    data setup of habitable planet

--- server/src/routes/planets/planets.controller.js
+++ server/src/routes/planets/planets.controller.js
@@ -1,4 +1,4 @@
-const planets = require("../../models/planets.model");
+const {planets} = require("../../models/planets.model");
 
 const getAllPlanets =  function getAllPlanets(req, res) {
   res.status(200).json(planets);
--- server/src/server.js
+++ server/src/server.js
@@ -1,12 +1,18 @@
 const http = require('http');
+const { start } = require('repl');
 const app = require('./app');
+const { loadPlanetsData } = require('./models/planets.model');
+const planetsModel = require('./models/planets.model');
 
 const PORT = process.env.PORT || 8000;
 
 const server = http.createServer(app);
-
-server.listen(PORT, () => {
-    console.log(`Listenning on port ${PORT}..`);
-});
+async function startServer(){
+    await loadPlanetsData();
+    server.listen(PORT, () => {
+        console.log(`Listenning on port ${PORT}..`);
+    });
+}
+startServer();
 
 
