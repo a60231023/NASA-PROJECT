c9e9bd68dba80885d64fb4bc539c2a25deadc1c8
a60231023 <ayushtiwari6061@gmail.com>
2023-03-06 19:35:29 +0530

    adding validation

--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -6,7 +6,19 @@ function getAllLaunches(req, res) {
 
 function httpAddNewLaunch(res, req) {
   const launch = req.body;
+  if(!launch.mission || !launch.rocket || !launch.launchDate || !launch.destination){
+    return res.status(400).json({
+        success: false,
+        error: 'missing required launch property',
+        message: 'enter all the details',
+    });
+  }
   launch.launchDate = new Date(launch.launchDate);
+  if(isNaN(launch.launchDate)){
+    return res.status(400).json({
+        error: 'invalid launch date'
+    });
+  }
   addNewLaunch(launch);
   return res.status(201).json(launch);
 }
